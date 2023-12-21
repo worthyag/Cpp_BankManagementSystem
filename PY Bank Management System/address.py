d07a1efda1cc49df45937564ae87c977de2e1c4e
Worthy <worthyintech@gmail.com>
2023-12-21 15:40:07 +0000

    Moving the python project to its own repository.

--- PY Bank Management System/address.py
+++
@@ -1,9 +0,0 @@
-class Address:
-    def __init__(self, country: str, state: str, city: str,
-                 street_address: str, post_code: str) -> None:
-        self.country: str = country
-        self.state: str = state
-        self.city: str = city
-        self.street_address: str = street_address
-        self.post_code: str = post_code
-        self.user_id: str = None
--- PY Bank Management System/main.py
+++
@@ -1,5 +0,0 @@
-from display import Display
-
-if __name__ == "__main__":
-    display = Display()
-    display.start_bank()
